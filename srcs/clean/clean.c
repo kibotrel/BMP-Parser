e9409fab392bd5ff7b3f52bdb751230abb76818b
BOTREL Kilian <botrel.kilian1999@gmail.com>
2019-09-30 21:20:26 +0200

    Addded cleaning process upon error or end of process

---
+++ srcs/clean/clean.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clean.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/30 19:38:46 by kibotrel          #+#    #+#             */
+/*   Updated: 2019/09/30 21:19:54 by kibotrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdint.h>
+#include "bmp.h"
+
+int	wipe(t_file *file, uint32_t status)
+{
+	if (file->stream)
+		free(file->stream);
+	return (dislay_output(status));
+}
