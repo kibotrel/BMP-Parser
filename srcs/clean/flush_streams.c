7ae314517cc4cbb0a10a0d9444ec716ca51bbd12
BOTREL Kilian <botrel.kilian1999@gmail.com>
2019-10-03 10:33:16 +0200

    Removed free that caused problem outside of the parser scope

--- srcs/clean/flush_streams.c
+++ srcs/clean/flush_streams.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 19:38:46 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/10/03 05:09:36 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/10/03 10:32:41 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@
 
 int	wipe(t_file *file, uint32_t status)
 {
-	if (file->pixels)
-		free(file->pixels);
 	if (file->stream)
 		free(file->stream);
 	return (dislay_output(file, status));
